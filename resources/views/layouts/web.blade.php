ba164106d587652f6cb7ed7ed3b5a4db951f9a55
Alexis Torres <axeltux@gmail.com>
2021-11-10 17:38:47 -0600

    Clase 9

---
+++ resources/views/layouts/web.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cursos de programación web</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <header class="shadow-lg">
+        <div class="bg-blue-900 py-1"></div>
+        <nav class="bg-blue-800 py-2">
+            <a href="{{ route('home') }}">
+                <img src="{{ asset('images/isotipo-fedora.png') }}" class="h-8 mx-auto" alt="Fedora">
+            </a>
+        </nav>
+    </header>
+    <main class="py-10">
+        <div class="container mx-auto px-4">
+            @yield('content')
+        </div>
+    </main>
+    <footer class="py-4 text-center">
+        @auth
+            <a href="{{ url('_dashboard') }}" class="text-sm text-gray-700 underline">
+                Dashboard
+            </a>
+        @else
+            <a href="{{ url('login') }}" class="text-sm text-gray-700 underline">
+                Login
+            </a>
+            <a href="{{ url('register') }}" class=" ml-4 text-sm text-gray-700 underline">
+                Register
+            </a>
+        @endif
+    </footer>
+</body>
+</html>
